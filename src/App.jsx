60707878ead977811df9fe4a005ca41c2040d9ad
Henry Narzary <narzaryhenry0@gmail.com>
2025-02-14 17:39:25 +0530

    testing and learning stage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { getPosts } from "./api/PostApi";
+
 const App = () => {
+  const getPostData = async () => {
+    const res = await getPosts();
+    console.log(res.data);
+  };
+
+  useEffect(() => {
+    getPostData();
+  }, []);
   return <div></div>;
 };
 
